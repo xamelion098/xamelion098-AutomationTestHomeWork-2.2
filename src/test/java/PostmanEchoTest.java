2db99fb975981bf3617a767de5694d981a5b5247
Aleksandr Stupnikov <a-a-stupnikov@yandex-team.ru>
2024-09-17 15:10:08 +0300

    PostmanPositiveTest

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -17,7 +17,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data" ,equalTo("Привет,мир!"))
+                .body("data" ,equalTo("Hello world!"))
         ;
     }
 }
